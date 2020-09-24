7165fde6e5cbc18ba3170b54d10ac5acd1ea8ebb
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-09-24 19:04:04 +0300

    что то считает

---
+++ app.js
@@ -0,0 +1,57 @@
+// Выбираем все кнопки на странице и получаем массив
+const btns = document.querySelectorAll("button");
+const answer = document.getElementsByClassName("answer")[0];
+const equal = document.getElementsByClassName("equal")[0];
+let result = 0;
+let newCalc = true;
+
+const isNumber = function (char) {
+  return Number.isInteger(Number.parseInt(char));
+};
+
+// Проходим по массиву
+btns.forEach(function (btn) {
+  // Вешаем событие клик
+  btn.addEventListener("click", function (e) {
+    const currentValue = e.target.value;
+
+    //проверим не число ли введено
+    if (isNumber(currentValue)) {
+      numberFunction(currentValue);
+      newCalc = false;
+    } else if (currentValue === "back") {
+      answer.textContent = answer.textContent.slice(0, -1);
+    } else if (currentValue === "ac") {
+      answer.textContent = "0";
+      newCalc = true;
+    } else if (currentValue === "%") {
+      answer.textContent = result / 100;
+    } else if (currentValue === "=") {
+      answer.textContent = eval(answer.textContent);
+      newCalc = true;
+    } else if (currentValue === "ex") {
+      console.log(currentValue);
+    } else if (currentValue === ".") {
+      console.log(currentValue);
+    } else {
+      const lastSymbol = answer.textContent[answer.textContent.length - 1];
+
+      if (isNumber(lastSymbol)) {
+        answer.textContent += currentValue;
+      }
+    }
+    writeEqual();
+  });
+});
+
+const numberFunction = function (number) {
+  answer.textContent = newCalc ? number : answer.textContent + number;
+};
+
+const writeEqual = function () {
+  const lastSymbol = answer.textContent[answer.textContent.length - 1];
+
+  if (isNumber(lastSymbol)) {
+    equal.textContent = "= " + eval(answer.textContent);
+  }
+};
